c9fea9f43869a3d0d7a8b7d02c1ef3b947b56540
Mario Ortega <mabel010830@gmail.com>
2023-07-09 18:24:26 -0400

    New  structure of the program

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 from fastapi import FastAPI
 from typing import Union
-from pydantic import BaseModel
+
 
 #Creacion de una aplicacion FastAPI basica:
 app = FastAPI()
 
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
     
 @app.get("/")
 def read_root():
