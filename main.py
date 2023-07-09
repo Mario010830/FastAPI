ce8a9dc9114a84795644493081acb767170f4245
Mario Ortega <mabel010830@gmail.com>
2023-07-09 18:02:58 -0400

    Creating the model Item with Pydantic

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
 from typing import Union
+from pydantic import BaseModel
 
 #Creacion de una aplicacion FastAPI basica:
 app = FastAPI()
 
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
 @app.get("/")
 def read_root():
     return {"Hello":"World"}
