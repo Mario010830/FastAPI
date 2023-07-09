670ff7f02bef377ed58fa723308144835398e9ca
Mario Ortega <mabel010830@gmail.com>
2023-07-09 18:18:33 -0400

    Created the route PUT to update an Item

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ class Item(BaseModel):
     name: str
     price: float
     is_offer: Union[bool, None] = None
+    
 @app.get("/")
 def read_root():
     return {"Hello":"World"}
@@ -25,3 +26,7 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @app.get("/calculadora")
 def calcular(operando_1: float, operando_2:float):
     return {"suma":operando_1 + operando_2}
+
+@app.put("/items/{item_id}")
+def update_item(item_id:int, item: Item):
+    return {"item_name": item.name, "item_id": item_id, "item_price": item.price}
