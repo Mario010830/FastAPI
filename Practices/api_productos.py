643aea4deba532e456697ec393b46239eef08fa6
Mario Ortega <mabel010830@gmail.com>
2023-07-11 22:34:37 -0400

    Created class Producto

--- Practices/api_productos.py
+++ Practices/api_productos.py
@@ -1,4 +1,15 @@
 from fastapi import FastAPI
+from typing import Optional
+
+from pydantic import BaseModel
+
+class Producto(BaseModel):
+    id: Optional[str]
+    nombre: str
+    precio_compra: float
+    precio_venta: float
+    proveedor: str
+    
 
 app = FastAPI()
 
